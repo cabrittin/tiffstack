edfb932db7ba323a57a7daa00bf134dcf266fe19
Christopher Brittin <cabrittin@gmail.com>
2024-11-13 15:33:21 -0500

    update

--- tiffstack/viewer.py
+++ tiffstack/viewer.py
@@ -7,8 +7,167 @@ Module for viewing tiff stacks in a VIM like editor
 @email: "cabrittin"+ <at>+ "gmail"+ "."+ "com"
 @date: 2019-12-05              
 """
-
+import numpy as np
 import cv2
+
+MAX_PIXEL = 2**16 - 1
+
+class Viewer:
+    def __init__(self,D):
+        """
+        Parameters:
+        ----------
+        D: Generic dataset class
+        """
+        
+        self.D = D
+        self.jdx = 0
+        self.idx = 0
+    
+    def load_dims(self):
+        self.sequence_size = self.D.get_sequence_size()
+        self.stack_size = self.D.get_stack_size()
+        self.shape = self.D.get_shape()
+ 
+    def preprocess(self):
+        self.load_dims()
+        self.pxmin = self.D.get_pxmin()
+        self.pxmax = self.D.get_pxmax()
+        self.pxlut = compute_lut(self.pxmin,self.pxmax)
+    
+    def on_close(self):
+        pass
+
+    def init_window(self):
+        self.win ='Stack'
+        cv2.namedWindow(self.win)
+        cv2.moveWindow(self.win,800,500)
+        self.update_title()
+
+    def update_title(self):
+        #wtitle = self.D.get_window_title(self.jdx,self.idx)
+        wtitle = self.get_current_title() 
+        cv2.setWindowTitle(self.win,wtitle)
+    
+    def get_current_title(self):
+        return 'Window'
+
+    def get_start_indicies(self):
+        return self.jdx,self.idx
+
+    def load_stack(self,jdx):
+        self.jdx = jdx
+
+    def get_sequence_size(self):
+        return self.sequence_size
+
+    def get_stack_size(self):
+        return self.stack_size
+    
+    def map_uint16_to_uint8(self,img):
+        """
+        Maps image from uint16 to uint8
+        """
+        return self.pxlut[img].astype(np.uint8)
+    
+    def display(self,idx):
+        self.idx = idx 
+        self.update_title()
+        img  = self.get_current_image()
+        return self.map_uint16_to_uint8(img)
+    
+    def get_current_image(self):
+        return self.D.get_image(self.idx,self.jdx)
+
+    def user_update(self,key,sequence_jdx,stack_idx):
+        jdx = sequence_jdx
+        idx = stack_idx
+
+        if key == ord('b'):
+            self.pxmax = max(self.pxmin,self.pxmax-100)
+            self.pxlut = compute_lut(self.pxmin,self.pxmax)
+            update_display(self)
+        
+        elif key == ord('t'):
+            self.pxmax = min(MAX_PIXEL,self.pxmax+100)
+            self.pxlut = compute_lut(self.pxmin,self.pxmax)
+            update_display(self)
+        
+        elif key == ord('v'):
+            self.pxmax = max(self.pxmin,self.pxmax-1)
+            self.pxlut = compute_lut(self.pxmin,self.pxmax)
+            update_display(self)
+        
+        elif key == ord('r'):
+            self.pxmax = min(MAX_PIXEL,self.pxmax+1)
+            self.pxlut = compute_lut(self.pxmin,self.pxmax)
+            update_display(self)
+ 
+        elif key == ord('w'):
+            self.pxmin = min(self.pxmax,self.pxmin+100)
+            self.pxlut = compute_lut(self.pxmin,self.pxmax)
+            update_display(self)
+        
+        elif key == ord('x'):
+            self.pxmin = max(0,self.pxmin-100)
+            self.pxlut = compute_lut(self.pxmin,self.pxmax)
+            update_display(self)
+        
+        elif key == ord('e'):
+            self.pxmin = min(self.pxmax,self.pxmin+1)
+            self.pxlut = compute_lut(self.pxmin,self.pxmax)
+            update_display(self)
+        
+        elif key == ord('c'):
+            self.pxmin = max(0,self.pxmin-1)
+            self.pxlut = compute_lut(self.pxmin,self.pxmax)
+            update_display(self)
+       
+        self._user_update(key)
+    
+    def _user_update(self,key):
+        pass
+
+def compute_lut(pxmin,pxmax):
+    pxlut = np.concatenate([
+        np.zeros(pxmin, dtype=np.uint16),
+        np.linspace(0,255,pxmax - pxmin).astype(np.uint16),
+        np.ones(MAX_PIXEL - pxmax, dtype=np.uint16) * 255
+        ])
+    
+    return pxlut
+
+def update_display(T):
+    cout = f'''
+        Sequence length: {T.sequence_size}
+
+        Keys:
+        -----
+        Quit: q 
+
+        Next (prev) timepoint: l(h)
+        Next (prev) z-slice: k(j)
+        
+        Raise max pixel +100 (+1): t(r)
+        Lower max pixel -100 (-1): b(v)
+        Raise min pixel +100 (+1): w(e)
+        Lower min pixel -100 (-1): x(c)
+
+        Timelapse:
+        ----------
+        Max pixel = {T.pxmax} 
+        Min pixel = {T.pxmin} 
+        
+        '''
+ 
+    LINE_UP = '\033[1A'
+    LINE_CLEAR = '\x1b[2K'
+    
+    loop = range(1, len(cout) + 1)
+    for idx in reversed(loop): print(LINE_UP, end=LINE_CLEAR)
+
+    print(cout,end='\r')
+
 
 def image_looper(S,large_iter=100,reset_idx=False):
     jdx,idx = S.get_start_indicies()  
